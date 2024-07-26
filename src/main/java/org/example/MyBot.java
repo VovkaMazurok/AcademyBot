757569a6273cd33f960999315e05b73a210350f8
Volodymyre <vova89.89@i.ua>
2024-07-26 21:09:34 +0300

    lessons 3

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -4,7 +4,6 @@ import net.thauvin.erik.crypto.CryptoPrice;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyBot extends TelegramLongPollingBot {
 
